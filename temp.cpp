62c6dd943536629db7b74a520e653c7e289a474c
axf <1460014874@qq.com>
2024-03-15 22:12:30 +0800

    增加Ubuntu下的配置文件

--- temp.cpp
+++ temp.cpp
@@ -8,13 +8,22 @@ const ll MAX = 2e5 + 10;
 const ll MOD = 1e9 + 7;
 int tx[] = {0, 0, 1, -1};
 int ty[] = {1, -1, 0, 0};
+map<string,int>mp;
 void solve()
 {
-    
+    string s;
+    cin>>s;
+    mp[s]++;
+    mp["safa"]=1;
+    cout<<1;
+    cout<<1;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
 }
 int main()
 {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    //ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     srand(time(nullptr));
     int t = 1;
     // cin >> t;
