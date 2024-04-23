2c61ae5077276bc453ecf4f89fb7b3be368d4056
axf <1460014874@qq.com>
2024-04-23 18:35:00 +0800

    ARC176 A 数据加强,原算法有问题,重写

--- AtCoder/ARC176/A_01_Matrix_Again.cpp
+++ AtCoder/ARC176/A_01_Matrix_Again.cpp
@@ -12,70 +12,35 @@ void solve()
 {
     int n, m;
     cin >> n >> m;
-    vector<pii> di(m);
-    set<int> dx, dy;
-    for (int i = 0; i < m; ++i)
-    {
-        cin >> di[i].first >> di[i].second;
-        dx.insert(di[i].first);
-        dy.insert(di[i].second);
-    }
-    for (int i = 1; i <= n; ++i)
-    {
-        if (dx.size() >= m)
-            break;
-        if (dx.find(i) == dx.end())
-            dx.insert(i);
-    }
-    for (int i = 1; i <= n; ++i)
-    {
-        if (dy.size() >= m)
-            break;
-        if (dy.find(i) == dy.end())
-            dy.insert(i);
-    }
-    vector<int> DX, DY;
-    for (auto v : dy)
-        DY.push_back(v);
-    for (auto v : dx)
-        DX.push_back(v);
-
+    vector<int> vis(n);
     vector<pii> ans;
-    for (int i = 0; i < m; ++i)
-        for (int j = 0; j < m; ++j)
-            ans.push_back({DX[i], DY[j]});
-
-    vector<set<int>> S(n + 1);
-    vector<int> row(n + 1, m), col(n + 1, m);
-    for (auto [a, b] : ans)
+    set<pii> s;
+    set<int> ss;
+    for (int i = 0, a, b; i < m; ++i)
     {
-        S[a].insert(b);
-        row[a]--;
-        col[b]--;
+        cin >> a >> b;
+        ans.emplace_back(a, b);
+        s.emplace(a, b);
+        vis[(a + b) % n] = 1;
+        ss.insert((a + b) % n);
     }
-
-    priority_queue<pii> Qc;
-    for (int i = 1; i <= n; ++i)
-        Qc.push({col[i], i});
-
-    for (int i = 1; i <= n; ++i)
+    for (int i = 0; i < n; ++i)
     {
-        vector<pii> tmp;
-        while (row[i] != 0)
+        if (ss.size() >= m)
+            break;
+        if (ss.find(i) == ss.end())
+            ss.insert(i);
+    }
+    for (auto v : ss)
+    {
+        for (int i = 1; i <= n; ++i)
         {
-            tmp.push_back(Qc.top());
-            Qc.pop();
-            if (S[i].find(tmp.back().second) != S[i].end())
-                continue;
-            ans.push_back({i, tmp.back().second});
-            S[i].insert(tmp.back().second);
-            tmp.back().first--;
-            row[i]--;
-        }
-        for (int j = 0; j < tmp.size(); ++j)
-        {
-            if (tmp[j].first != 0)
-                Qc.push(tmp[j]);
+            int tmp = i < v ? v - i : (n - i + v == 0 ? n : n - i + v);
+            if (s.find({tmp, i}) == s.end())
+            {
+                s.insert({tmp, i});
+                ans.push_back({tmp, i});
+            }
         }
     }
     cout << ans.size() << endl;
