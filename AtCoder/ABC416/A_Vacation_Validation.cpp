22ef9f0f9dd96d36e4bc9bc1e7390b3d3a4c2f18
axf <1460014874@qq.com>
2025-07-26 20:58:59 +0800

    'ABC416'

---
+++ AtCoder/ABC416/A_Vacation_Validation.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> make1(string strArr);
+vector<vector<int>> make2(string strArr);
+#define endl '\n'
+// leetcode--------start
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define pii pair<int, int>
+int tx[] = {0, 0, 1, -1};
+int ty[] = {1, -1, 0, 0};
+ll MOD = 1e9 + 7;
+class Solution
+{
+public:
+     int beautifulSplits(vector<int> &nums)
+     {
+          return 1;
+     }
+};
+// leetcode--------end
+// void solve()
+// {
+//     int n = 0;
+//     int k = 0;
+//     int q = 0;
+//     vector<int> A = make1("[1,2,3,1,2,3,1,2,3]");
+//     vector<int> B = make1("");
+//     vector<vector<int>> AA = make2("[[1,2],[2,5],[3,9],[1,15]]");
+//     vector<vector<int>> BB = make2("");
+//     Solution t;
+//     // cout << t.beautifulSplits(A);
+// }
+
+void solve()
+{
+     int n, l, r;
+     cin >> n >> l >> r;
+     string str;
+     cin >> str;
+     string tmp;
+     for (int i = 0; i < r - l + 1; ++i)
+          tmp.push_back('o');
+     if (str.substr(l - 1, r - l + 1) == tmp)
+          cout << "Yes";
+     else
+          cout << "No";
+}
+
+int main()
+{
+     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+     srand(time(nullptr));
+     int t = 1;
+     // cin >> t;
+     while (t--)
+     {
+          solve();
+     }
+     return 0;
+}
+
+vector<int> make1(string strArr)
+{
+     vector<int> result;
+     if (strArr.empty())
+          return result;
+     string item;
+     strArr = strArr.substr(1, strArr.size() - 2);
+     stringstream stream(strArr);
+     while (getline(stream, item, ','))
+     {
+          result.push_back(stoi(item));
+     }
+     return result;
+}
+vector<vector<int>> make2(string strArr)
+{
+     vector<vector<int>> result;
+     if (strArr.empty())
+          return result;
+     string item;
+     strArr = strArr.substr(2, strArr.size() - 4);
+     stringstream stream(strArr);
+     bool first = true;
+     while (getline(stream, item, ']'))
+     {
+          if (!item.empty())
+          {
+               if (!first)
+                    item = item.substr(2);
+               vector<int> innerResult = make1("[" + item + "]");
+               result.push_back(innerResult);
+               first = false;
+          }
+     }
+     return result;
+}
