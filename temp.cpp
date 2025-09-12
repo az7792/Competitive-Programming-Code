b8ebcca67f5fde089a474a0612653d2427113f23
axf <1460014874@qq.com>
2025-09-12 22:56:04 +0800

    区分acm与leetcode初始模板

--- temp.cpp
+++ temp.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> make1(string strArr);
-vector<vector<int>> make2(string strArr);
 #define endl '\n'
-// leetcode--------start
 #define ll long long
 #define ull unsigned long long
 #define all(x) (x).begin(), (x).end()
@@ -31,26 +28,8 @@ std::ostream &operator<<(std::ostream &_out, std::vector<T> &_a)
     return _out;
 }
 
-class Solution
-{
-public:
-    int beautifulSplits(vector<int> &nums)
-    {
-        return 1;
-    }
-};
-// leetcode--------end
 void solve()
 {
-    int n = 0;
-    int k = 0;
-    int q = 0;
-    vector<int> A = make1("[1,2,3,1,2,3,1,2,3]");
-    vector<int> B = make1("");
-    vector<vector<int>> AA = make2("[[1,2],[2,5],[3,9],[1,15]]");
-    vector<vector<int>> BB = make2("");
-    Solution t;
-    // cout << t.beautifulSplits(A);
 }
 int main()
 {
@@ -63,41 +42,4 @@ int main()
         solve();
     }
     return 0;
-}
-
-vector<int> make1(string strArr)
-{
-    vector<int> result;
-    if (strArr.empty())
-        return result;
-    string item;
-    strArr = strArr.substr(1, strArr.size() - 2);
-    stringstream stream(strArr);
-    while (getline(stream, item, ','))
-    {
-        result.push_back(stoi(item));
-    }
-    return result;
-}
-vector<vector<int>> make2(string strArr)
-{
-    vector<vector<int>> result;
-    if (strArr.empty())
-        return result;
-    string item;
-    strArr = strArr.substr(2, strArr.size() - 4);
-    stringstream stream(strArr);
-    bool first = true;
-    while (getline(stream, item, ']'))
-    {
-        if (!item.empty())
-        {
-            if (!first)
-                item = item.substr(2);
-            vector<int> innerResult = make1("[" + item + "]");
-            result.push_back(innerResult);
-            first = false;
-        }
-    }
-    return result;
 }
